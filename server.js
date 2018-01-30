8d9ce9663a51a7719a143bfe30ee5433229ffd76
PNKBizz <bizz.gc@gmail.com>
2018-01-30 21:49:51 +0300

    work with app

--- server.js
+++ server.js
@@ -6,11 +6,19 @@ const app = express()
 const cloudName = 'rzntattoo'
 const apiKey = '842863162776522'
 const apiSecret = '54iNJbmcDbgEWJsBnCBF1jW5r8U'
+const masters = [{name: 'Швед', folder: 'shwed'}]
 
 app.use(express.json())
 app.use(cors())
 
 app.use(cloudinaryUpload(cloudName, apiKey, apiSecret))
+
+app.get('/api/getMasters', (req, res) => res.json(masters));
+
+app.post('/api/setMaster', (req, res) => {
+    masters.push(req.body)
+    res.json(masters)
+})
 
 app.use(express.static(`${__dirname}/`))
 app.listen(process.env.PORT || 4002, () => {
